b42c6e6f04dae7c217cd1302f211ba065f6e4fdc
takatoshi.tenko <chanten811@gmail.com>
2023-02-28 00:23:03 +0900

    Tailwind CSSの調整

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  },
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    // For the best performance and to avoid false positives,
-    // be as specific as possible with your content configuration.
-  ]
+  }
 };
