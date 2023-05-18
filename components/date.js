5d1938f5a8617a6646298b0b16c73cbb97bfbd17
takatoshi.tenko <chanten811@gmail.com>
2023-05-19 01:36:33 +0900

    フォルダ構成を作り替える

--- components/date.js
+++
@@ -1,9 +0,0 @@
-import { parseISO, format } from 'date-fns';
-
-export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  if (Number.isNaN(date.getTime())) {
-    return
-  }
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
-}
